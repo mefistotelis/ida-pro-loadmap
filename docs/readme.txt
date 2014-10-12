c8c312329dd9b70668b7a6f654821247a2e199d3
mefistotelis@gmail.com <mefistotelis@gmail.com@01266e12-6a20-0eb8-caed-66ab7311f222>
2014-10-12 22:25:54 +0000

    Sources from v1.1.

---
+++ docs/readme.txt
@@ -0,0 +1,10 @@
+IDA Pro plugin
+
+compiled with IDA 6.1 SDK, compiler - GCC 4.5.0
+
+See src/LoadMap.cpp for credits, license and changelog.
+
+Installation:
+* Copy LoadMAP.plw to IDA plugins folder
+* Open any PE/LE file project
+* Click Load MAP with Shift to see options
---
+++ src/stdafx.h
@@ -0,0 +1,64 @@
+////////////////////////////////////////////////////////////////////////////////
+/// @file stdafx.h
+///     Include file for standard system include files.
+/// @par Purpose:
+///     Includes standard system headers, or project specific include files
+///     that are used frequently, but are changed infrequently.
+/// @author TQN <truong_quoc_ngan@yahoo.com>
+/// @author TL <mefistotelis@gmail.com>
+/// @date 2004.09.11 - 2011.09.13
+/// @par  Copying and copyrights:
+///     This program is free software; you can redistribute it and/or modify
+///     it under the terms of the GNU General Public License as published by
+///     the Free Software Foundation; either version 2 of the License, or
+///     (at your option) any later version.
+////////////////////////////////////////////////////////////////////////////////
+#ifndef STDAFX_H_
+#define STDAFX_H_
+
+#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+#define _OBJC_NO_COM
+//#define WINSHLWAPI
+#define NOGDI
+#include <windef.h>
+#include <basetyps.h>
+#include <cstdarg>
+#include <winbase.h>
+#include <winuser.h>
+//#include <windows.h>
+
+// C RTL Debug Support Header Files
+//#include <crtdbg.h>
+
+// Shell Lightweight API
+#include <winreg.h>
+#include <shlwapi.h>
+#pragma comment(lib, "shlwapi.lib")
+
+#define USE_STANDARD_FILE_FUNCTIONS
+#define USE_DANGEROUS_FUNCTIONS
+
+// IDA SDK Header Files
+#include <ida.hpp>
+#include <idp.hpp>
+#include <loader.hpp>
+#include <bytes.hpp>
+#include <name.hpp>
+#include <entry.hpp>
+#include <fpro.h>
+
+#ifdef _DEBUG
+    #define _VERIFY(x)  _ASSERTE(x)
+
+    #define WIN32CHECK(x)   { \
+        DWORD __dwErr__ = GetLastError(); \
+        _ASSERTE(x); \
+        SetLastError(__dwErr__); \
+    };
+#else
+    #define _VERIFY(x)  (x)
+    #define WIN32CHECK(x)   (x)
+#endif
+
+#endif //!STDAFX_H_
+////////////////////////////////////////////////////////////////////////////////
